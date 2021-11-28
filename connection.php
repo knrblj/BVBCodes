1a370b364c975ba174e121f928f1490265bd4ae6
Koneru Balaji <75683560+knrblj@users.noreply.github.com>
2021-11-28 20:56:57 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-	$conn=mysqli_connect('localhost','knrbljxy','O:Zi9jD32uA.m2','knrbljxy_project');
+	$conn=mysqli_connect('ec2-23-23-219-25.compute-1.amazonaws.com','fwphcrgoudrxmm','d6d15bf3877fdbcc7f7cda3609f1ca9bbbc08bf00f914bde559cccef3b81a80e','knrbljxy_project');
 	if(!$conn)
 	{
 		echo "Unable to connect";
